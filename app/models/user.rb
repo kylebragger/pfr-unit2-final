d2902c8edb0721a82bb0689b2eb106baae3576ea
Kyle Bragger <kyle.bragger@gmail.com>
2012-10-12 10:47:36 -0700

    lesson 4: validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_digest, :username
+  
+  validates :email, :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: { on: :create }, length: { minimum: 6 }
+  validates :username, length: { in: 1..40 }
+  validates :email, format: /@/
+  validates :username, format: /\A([a-z])([_a-z]*)\z/i
+  validate :check_reserved_usernames
+  
+  protected
+  
+  def check_reserved_usernames
+    reserved = %w{admin kyle leo}
+    self.errors.add(:username, 'is not allowed') if reserved.include?(self.username.downcase)
+  end
 end
